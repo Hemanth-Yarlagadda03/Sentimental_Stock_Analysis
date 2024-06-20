2aff415f37d08fb1a9680d721d28d0ac48ebbeaa
Venkat-Kowshik <kowshikgowrakkavari@gmail.com>
2024-06-21 01:56:41 +0530

    Upload Successful

---
+++ myapp/models.py
@@ -0,0 +1,8 @@
+
+
+# Create your models here.
+from django.db import models
+
+class Upload(models.Model):
+    file = models.FileField(upload_to='uploads/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
